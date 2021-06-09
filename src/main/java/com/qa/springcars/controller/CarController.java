0a6413f2160f5b9307072f4f7e59897952f4332e
savannahvaith <sav.vaithilingam@gmail.com>
2021-06-09 20:01:31 +0100

    SV: Added Response Entities to Controllers

--- src/main/java/com/qa/springcars/controller/CarController.java
+++ src/main/java/com/qa/springcars/controller/CarController.java
@@ -3,6 +3,8 @@ package com.qa.springcars.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,31 +33,39 @@ public class CarController {
 	// CREATE
 	
 	@PostMapping("/create")
-	public String createVehicle(@RequestBody Car car){
+	public ResponseEntity<Car> createVehicle(@RequestBody Car car){
 		this.vehicles.add(car);
-		return "Successfully Added Car!";
+		// Grab the last thing that was added to the list
+		Car added = this.vehicles.get(this.vehicles.size()-1);
+		return new ResponseEntity<Car>(added, HttpStatus.CREATED);
 	}
 	
 	// READ
 	
 	@GetMapping("/getAll")
-	public List<Car> getAllVehicles(){
-		return this.vehicles; 
-	}
-	
-	// DELETE ONE
-	
-	@DeleteMapping("/remove/{index}")
-	public Car removeVehicle(@PathVariable int index) {
-		return this.vehicles.remove(index);
+	public ResponseEntity<List<Car>> getAllVehicles(){
+		//return new ResponseEntity<List<Car>>(this.vehicles,HttpStatus.OK); 
+		// builder pattern 
+		return ResponseEntity.ok(this.vehicles);
+		
 	}
 	
 	// READ ONE
 	
 	@GetMapping("/getOne/{index}")
-	public Car getVehicle(@PathVariable int index) {
-		return this.vehicles.get(index);
+	public ResponseEntity<Car> getVehicle(@PathVariable int index) {
+		return ResponseEntity.ok(this.vehicles.get(index));
 	}
+	
+	
+	// DELETE ONE
+	
+	@DeleteMapping("/remove/{index}")
+	public ResponseEntity removeVehicle(@PathVariable int index) {
+		return this.vehicles.remove(index) != null ? new ResponseEntity<>(HttpStatus.NO_CONTENT) :
+			new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 	
 
 }
