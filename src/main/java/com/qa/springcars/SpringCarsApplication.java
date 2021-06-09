e9be84879937331f02ae4a167283d3c386a90257
savannahvaith <sav.vaithilingam@gmail.com>
2021-06-09 16:18:44 +0100

    SV: Added Beans Example

--- src/main/java/com/qa/springcars/SpringCarsApplication.java
+++ src/main/java/com/qa/springcars/SpringCarsApplication.java
@@ -1,15 +1,28 @@
 package com.qa.springcars;
 
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCarsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCarsApplication.class, args);
+		ApplicationContext beanBag = SpringApplication.run(SpringCarsApplication.class, args);
+		
+		beanBag.containsBean("getCurrent");
 
 	}
-	
+
+	// By default Beans have a SINGLETON instance = only one instance that is
+	// continuously changed
+	// BEAN - Managed object By Spring
+	@Bean
+	public LocalTime getCurrent() {
+		return LocalTime.now();
+	}
 
 }
