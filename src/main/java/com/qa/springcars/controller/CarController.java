bbec21f737ca131b82297dee92db854393399486
savannahvaith <sav.vaithilingam@gmail.com>
2021-06-09 20:32:27 +0100

    SV: Added Update methods

--- src/main/java/com/qa/springcars/controller/CarController.java
+++ src/main/java/com/qa/springcars/controller/CarController.java
@@ -3,12 +3,16 @@ package com.qa.springcars.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +31,7 @@ public class CarController {
 	
 	// List - Create a new car - Add a car to a list
 	// Read a car from a list
-	// Update a car
+	// Update a car 
 	// Delete a car
 	
 	// CREATE
@@ -66,6 +70,22 @@ public class CarController {
 			new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	// PUT - replace the whole thing
+	@PutMapping("/replace/{index}")
+	public ResponseEntity<Car> updateVehicle(@PathVariable int index, @RequestBody Car newCar){
+		this.vehicles.set(index, newCar);
+		return new ResponseEntity<Car>(newCar, HttpStatus.ACCEPTED);
+	}
+	
+	// PATCH - Update a single bit of the data
+	@PatchMapping("/update/{index}")
+	public ResponseEntity<Car> changeAttribute(@PathParam("make") String make, @PathParam("model") String model, @PathVariable int index){
+		
+		Car updatedVehicle = this.vehicles.get(index);
+		updatedVehicle.setMake(make);
+		
+		return new ResponseEntity<Car>(updatedVehicle, HttpStatus.ACCEPTED);
+	}
 	
 
 }
