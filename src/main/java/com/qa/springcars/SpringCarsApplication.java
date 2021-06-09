5038799bb2c2e69bbd09440c6208434094714590
savannahvaith <sav.vaithilingam@gmail.com>
2021-06-09 16:04:25 +0100

    SV: Initial Commit

---
+++ src/main/java/com/qa/springcars/SpringCarsApplication.java
@@ -0,0 +1,15 @@
+package com.qa.springcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCarsApplication.class, args);
+
+	}
+	
+
+}
