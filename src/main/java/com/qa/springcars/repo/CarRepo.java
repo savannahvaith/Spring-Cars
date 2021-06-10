db274bb846621957090fc552d5c9548b047da3b7
savannahvaith <sav.vaithilingam@gmail.com>
2021-06-10 17:06:03 +0100

    SV: Created Interface for Repo

---
+++ src/main/java/com/qa/springcars/repo/CarRepo.java
@@ -0,0 +1,12 @@
+package com.qa.springcars.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.springcars.domain.Car;
+
+@Repository
+public interface CarRepo extends JpaRepository<Car,Long>{
+								//Car - Entity
+								//Long - ID Data Type
+}
